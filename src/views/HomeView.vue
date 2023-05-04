5e5fb1d835052e536aab4208e9397b86c643a8a1
王亚松 <2824289618@qq.com>
2023-05-04 15:39:56 +0800

    e

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>鹅鹅鹅</h1>
+    <h1>曲项向天歌</h1>
+    <h1>白毛浮绿水</h1>
+    <h1>红掌拨清波</h1>
   </div>
 </template>
 
@@ -10,6 +15,15 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  data() {
+    return {
+      info:'泰酷辣',
+
+      name:'mt',
+
+      age:'20'
+    }
+  },
   name: 'HomeView',
   components: {
     HelloWorld
